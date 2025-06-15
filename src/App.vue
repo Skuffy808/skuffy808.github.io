82438f42105be3a2e3efb41adde020907c702b41
Skuffy <skuffy808@gmail.com>
2025-06-15 21:54:35 +1200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container mx-auto p-5">
+    <h1 class="text-3xl font-bold underline bg-red-500 text-black">
+      Hello world!
+    </h1>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
