36acbe318de8077d6489590379c1e63edfaf15d8
Skuffy <skuffy808@gmail.com>
2025-06-16 13:14:15 +1200

    add About

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 import Hero from './components/Hero.vue';
 import Card from './components/Card.vue';
 import CategoryHeader from './components/CategoryHeader.vue';
+import About from './components/About.vue';
 </script>
 
 
 <template>
   <div class="min-h-screen bg-gradient-to-br from-blue-500 via-black to-pink-500 text-white flex flex-col items-center justify-center gap-8 px-4 py-12">
     <Hero />
+    <CategoryHeader title="About Me"/>
+    <About />
     <CategoryHeader title="Discord Servers" description="Some cool discord servers that I own"/>
     <Card card-title="Skuffy's Tavern" card-description="The personal discord server of the one and only Skuffy" card-image="skuffy.png" card-link="https://discord.gg/U5mH8wJBMk"/>
     <Card card-title="Game.dev()" card-description="Find people to collaborate with and make games!" card-image="game.dev().png" card-link="https://discord.gg/C8qgknw7H3"/>
---
+++ src/components/About.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-1/2 bg-white rounded-xl p-4 flex items-start gap-4">
+        <p class="text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, atque facilis dolorum quibusdam perspiciatis laborum, omnis consectetur minima tempore aliquid error. Quidem illo quibusdam temporibus ducimus omnis ex. Corrupti, voluptates?</p>
+    </div>
+</template>
